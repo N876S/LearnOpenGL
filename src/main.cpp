ee7e9814b0eca5a507a140e3f59c673b232dd228
Nathan Small <nsmall795@gmail.com>
2026-05-17 00:20:40 -0400

    Added textures

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "shader.h"
 #include <cmath>
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
@@ -44,11 +45,17 @@ int main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   float data[] = { 
-    //POSITION            COLOR
-    -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
-    0.0f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f,
-    0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f
+    //POSITION            COLOR               TEXTURE COORDS
+    -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
+    0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
+    0.5f, 0.5f, 0.0f,     1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f
   };
+
+  //create shader
+  Shader shader("../shaders/shader.vs", "../shaders/shader.fs");
 
   //VAO
   unsigned int VAO;
@@ -62,16 +69,39 @@ int main() {
   glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
 
   //attributes
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
   glEnableVertexAttribArray(1);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  //create texture
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  stbi_set_flip_vertically_on_load(true);
+  int tWidth, tHeight, nrChannels;
+  unsigned char* tData = stbi_load("../res/awesomeface.png", &tWidth, &tHeight, &nrChannels, 0);
+  if(tData != 0){
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tWidth, tHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, tData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(tData);
+  } else {
+    std::cout << "FAILED TO LOAD TEXTURE" << std::endl;
+  }
+  shader.use();
+  shader.setInt("textureData", 0);
+  glBindTexture(GL_TEXTURE_2D, 0);
 
   //unbind for later use
   glBindVertexArray(0);
-
-  //create shader
-  Shader shader("../shaders/shader.vs", "../shaders/shader.fs");
 
   //colour logic
   float time, colorAdjust;
@@ -90,11 +120,12 @@ int main() {
     colorAdjust = std::abs(std::sin(time*2.0f));
     shader.setFloat("colorAdj", colorAdjust);
 
-
     //draw triangles
     shader.use();
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     // poll events and swap buffers
     glfwPollEvents();
