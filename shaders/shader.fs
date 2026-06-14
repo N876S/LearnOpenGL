e01f23fa8b893d704bab7d1667bfebe0fec7f09f
NathanSmall <nsmall795@gmail.com>
2026-06-13 20:23:58 -0400

    spinny boys

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,12 +1,9 @@
 #version 460 core
 out vec4 fragColor;
-in vec3 colorPos;
 in vec2 texCoord;
-
-uniform float colorAdj;
 
 uniform sampler2D textureData;
 
 void main(){
-    fragColor = texture(textureData, texCoord) * vec4(colorPos.x*(1-colorAdj/2), colorPos.y*(1-colorAdj/2), colorPos.z*(1-colorAdj/2), 1.0f);
+    fragColor = texture(textureData, texCoord);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -48,14 +48,63 @@ int main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   float data[] = { 
-    //POSITION            COLOR               TEXTURE COORDS
-    -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-    -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-    0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-    0.5f, 0.5f, 0.0f,     1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
-    0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-    -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    //POSITION           TEXTURE COORDS
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,              //FACE 1
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,  
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,              //FACE 2
+    0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,          //FACE 3
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,           //FACE 4
+    0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f,            //FACE 5
+    0.5f, -0.5f, 0.5, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,             //FACE 6
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+    -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
   };
+
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f, 0.0f, 0.0f),
+    glm::vec3( 2.0f, 5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f, 3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f, 2.0f, -2.5f),
+    glm::vec3( 1.5f, 0.2f, -1.5f),
+    glm::vec3(-1.3f, 1.0f, -1.5f)
+  };
+
 
   //create shader
   Shader shader("../shaders/shader.vs", "../shaders/shader.fs");
@@ -72,12 +121,10 @@ int main() {
   glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
 
   //attributes
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
   glEnableVertexAttribArray(1);
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
-  glEnableVertexAttribArray(2);
 
   //create texture
   unsigned int texture;
@@ -109,6 +156,17 @@ int main() {
   //colour logic
   float time, colorAdjust;
 
+  glm::mat4 view = glm::mat4(1.0f);
+  view = glm::translate(view, glm::vec3(5.0f, 0.0f, -15.0f));
+  view = glm::rotate(view, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+  view = glm::rotate(view, glm::radians(30.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+  glm::mat4 proj = glm::mat4(1.0f);
+  proj = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 0.1f, 100.0f);
+
+  //enable depth buffer
+  glEnable(GL_DEPTH_TEST);
+
   // render loop
   while (!glfwWindowShouldClose(window)) {
     // input
@@ -116,7 +174,7 @@ int main() {
 
     //---------------------------render/draw--------------------------------------
     glClearColor(0.1f, 0.5f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     //update uniforms
     //colour updates
@@ -124,21 +182,26 @@ int main() {
     colorAdjust = std::abs(std::sin(time*2.0f));
     shader.setFloat("colorAdj", colorAdjust);
 
-    //movement update
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::scale(trans, glm::vec3(sin(time)*0.5f, sin(time)*0.5f, sin(time)*0.5f));
-    trans = glm::rotate(trans, glm::radians((float)sin(time)*180.0f), glm::normalize(glm::vec3(0.5f, 0.7f, 0.5f)));
-    shader.setMatrix4f("trans", trans);
-
     //3d updates
-    
+    //create 3d matrices
+
+    shader.setMatrix4f("view", view);
+
+    shader.setMatrix4f("proj", proj);
+
 
     //draw triangles
     shader.use();
     glBindVertexArray(VAO);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    for(glm::vec3 position : cubePositions){
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, position);
+      model = glm::rotate(model, glm::radians((float)sin(time)*1000.0f), glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f)));
+      shader.setMatrix4f("model", model);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     // poll events and swap buffers
     glfwPollEvents();
